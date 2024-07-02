9c45319e278aba5e9ca420f656a3741452f11364
Roselyn Manegene <107440478+rozie321@users.noreply.github.com>
2024-07-02 11:33:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//challenge1
+let Challenge1buttonElement = document.querySelector("#challenge-1");
+Challenge1buttonElement.setAttribute("disabled", "disabled");
+
+//challenge2
+let Challenge2buttonElement = document.querySelector("#challenge-2");
+Challenge2buttonElement.style.background = "green";
+
+//challenge3
+let Challenge3buttonElement = document.querySelector("#challenge-3");
+Challenge3buttonElement.classList = "danger";
